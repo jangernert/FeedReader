385615980c7c8e272c9b95a0652cfc3fb41deb86
Sam Thomas <sgpthomas@gmail.com>
2015-08-28 18:25:16 -0700

    Removed my build folder

--- build/po/CMakeFiles/i18n.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/i18n"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/i18n.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/po/CMakeFiles/pot.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/pot"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/pot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)

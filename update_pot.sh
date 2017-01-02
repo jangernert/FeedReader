a98ce177e73ffaf8b6634bef8f806789f53ca437
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-01-02 05:47:41 +0100

    fix translations, and update translation script

--- update_pot.sh
+++ update_pot.sh
@@ -1,1 +1,55 @@
-find . -name "*.vala" ! -name "FeedServer.vala" ! -name "grabber.vala" ! -name "grabberUtils.vala" ! -name "DebugUtils.vala" -o -name "*.ui" -o -name "*.appdata.xml.in" -o -name "*.desktop.in" | xargs xgettext -o po/FeedReader.pot
+#!/bin/sh
+DOMAIN=FeedReader
+BASEDIR=$(dirname $0)
+OUTPUT_FILE=${BASEDIR}/po/${DOMAIN}.pot
+
+echo "Extracting translatable strings... "
+
+# Extract the strings from D source code. Since xgettext does not support D
+# as a language we use Vala, which works reasonable well.
+find ${BASEDIR}/ -name '*.vala' | xgettext \
+  --output $OUTPUT_FILE \
+  --files-from=- \
+  --directory=$BASEDIR \
+  --language=Vala \
+  --keyword=C_:1c,2 \
+  --from-code=utf-8
+  --from-code=utf-8
+
+xgettext \
+  --join-existing \
+  --output $OUTPUT_FILE \
+  --default-domain=$DOMAIN \
+  --package-name=$DOMAIN \
+  --directory=$BASEDIR \
+  --foreign-user \
+  --language=Desktop \
+  ${BASEDIR}/data/feedreader.desktop.in
+
+xgettext \
+  --join-existing \
+  --output $OUTPUT_FILE \
+  --default-domain=$DOMAIN \
+  --package-name=$DOMAIN \
+  --directory=$BASEDIR \
+  --foreign-user \
+  --language=Desktop \
+  ${BASEDIR}/data/feedreader-autostart.desktop.in
+
+xgettext \
+  --join-existing \
+  --output $OUTPUT_FILE \
+  --default-domain=$DOMAIN \
+  --package-name=$DOMAIN \
+  --directory=$BASEDIR \
+  --foreign-user \
+  --language=appdata \
+  ${BASEDIR}/data/feedreader.appdata.xml.in
+
+# Merge the messages with existing po files
+echo "Merging with existing translations... "
+for file in ${BASEDIR}/po/*.po
+do
+  echo -n $file
+  msgmerge --update $file $OUTPUT_FILE
+done

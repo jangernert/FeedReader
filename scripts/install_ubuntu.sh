879f9c385074681bda4166b0d194e02f226ec379
Brendan Long <self@brendanlong.com>
2018-05-01 11:12:44 -0400

    Fix Ubuntu script
     - Install meson and ninja instead of cmake
     - Add -y back

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo add-apt-repository ppa:vala-team
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install build-essential meson ninja-build valac pkg-config libgirepository1.0-dev libgtk-3-dev libsoup2.4-dev libjson-glib-dev libwebkit2gtk-4.0-dev libsqlite3-dev libsecret-1-dev libnotify-dev libxml2-dev librest-dev libgee-0.8-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgoa-1.0-dev libcurl4-gnutls-dev libpeas-dev
+sudo apt-get upgrade -y
+sudo apt-get install -y build-essential git meson ninja-build valac pkg-config libgirepository1.0-dev libgtk-3-dev libsoup2.4-dev libjson-glib-dev libwebkit2gtk-4.0-dev libsqlite3-dev libsecret-1-dev libnotify-dev libxml2-dev librest-dev libgee-0.8-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgoa-1.0-dev libcurl4-gnutls-dev libpeas-dev
 cd /tmp/
 git clone --recursive https://github.com/jangernert/FeedReader
 cd ./FeedReader

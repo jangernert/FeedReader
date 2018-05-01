81ca47a26fb305b7a87343d72cb4171fce49e8fc
Brendan Long <self@brendanlong.com>
2018-05-01 10:04:10 -0400

    Update the install_ubuntu.sh script
    Use meson + ninja instead of cmake + make

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -2,13 +2,12 @@
 sudo add-apt-repository ppa:vala-team
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential cmake valac pkg-config libgirepository1.0-dev libgtk-3-dev libsoup2.4-dev libjson-glib-dev libwebkit2gtk-4.0-dev libsqlite3-dev libsecret-1-dev libnotify-dev libxml2-dev librest-dev libgee-0.8-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgoa-1.0-dev libcurl4-gnutls-dev libpeas-dev
+sudo apt-get install build-essential meson ninja-build valac pkg-config libgirepository1.0-dev libgtk-3-dev libsoup2.4-dev libjson-glib-dev libwebkit2gtk-4.0-dev libsqlite3-dev libsecret-1-dev libnotify-dev libxml2-dev librest-dev libgee-0.8-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgoa-1.0-dev libcurl4-gnutls-dev libpeas-dev
 cd /tmp/
 git clone --recursive https://github.com/jangernert/FeedReader
 cd ./FeedReader
-mkdir build
+meson build
 cd ./build
-cmake ..
-make
-sudo make install
+ninja
+sudo ninja install
 sudo rm -r /tmp/FeedReader

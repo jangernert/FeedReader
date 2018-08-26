4063832425fa93cac990accbdd4e686bd30e1195
Brendan Long <self@brendanlong.com>
2018-08-26 11:56:05 -0400

    libvilistextum: Add tests
    A lot of these are broken and commented out so far.
    
    I changed libvilistetum to add \n between lines, not just at the end
    of every line to make it easier to do "nothing should change" tests.
    
    Part of #741

---
+++ libraries/libVilistextum/test.c
@@ -0,0 +1,85 @@
+#include <glib.h>
+
+#include "vilistextum.h"
+
+static void test_changed_inputs_with_html(void)
+{
+	char* inputs[][2] = {
+		{"<h ", "<h"},
+		{"a\nb", "a b"},
+		{"a<br>b", "a<br>b"},
+		//{"test&nbsp;two", "test\xa0two"},
+		//{"test&#160;two", "test\xa0two"},
+		//{"test&#160two", "test\xa0two"}
+	};
+	for (size_t i = 0; i < sizeof(inputs) / sizeof(char*[2]); ++i)
+	{
+		char* input = inputs[i][0];
+		char* output = vilistextum(input, 0);
+		char* expect = inputs[i][1];
+		g_assert_cmpstr(output, ==, expect);
+	}
+}
+
+static void test_unchanged_inputs_with_html(void)
+{
+	char* inputs[] = {
+		"&",
+		"<",
+		"<h",
+		"<r \xfd\xbd\xbd\xbd\xbd\xbd",
+		"test&nbsptwo"
+	};
+	for (size_t i = 0; i < sizeof(inputs) / sizeof(char*); ++i)
+	{
+		char* input = inputs[i];
+		char* output = vilistextum(input, 0);
+		g_assert_cmpstr(output, ==, input);
+	}
+}
+
+static void test_changed_inputs_without_html(void)
+{
+	char* inputs[][2] = {
+		//{"<h ", "<h"},
+		{"a\nb", "a b"},
+		{"a<br/>b", "a\nb"},
+		//{"test&nbsp;two", "test\xa0two"},
+		//{"test&#160;two", "test\xa0two"},
+		//{"test&#160two", "test\xa0two"}
+	};
+	for (size_t i = 0; i < sizeof(inputs) / sizeof(char*[2]); ++i)
+	{
+		char* input = inputs[i][0];
+		char* output = vilistextum(input, 1);
+		char* expect = inputs[i][1];
+		g_assert_cmpstr(output, ==, expect);
+	}
+}
+
+static void test_unchanged_inputs_without_html(void)
+{
+	char* inputs[] = {
+		"&",
+		"<",
+		//"<h",
+		//"<r \xfd\xbd\xbd\xbd\xbd\xbd",
+		"test&nbsptwo"
+	};
+	for (size_t i = 0; i < sizeof(inputs) / sizeof(char*); ++i)
+	{
+		char* input = inputs[i];
+		char* output = vilistextum(input, 1);
+		g_assert_cmpstr(output, ==, input);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	g_test_init(&argc, &argv, NULL);
+	g_test_add_func("/vilistextum/unchanged-inputs/with-html", test_unchanged_inputs_with_html);
+	g_test_add_func("/vilistextum/changed-inputs/with-html", test_changed_inputs_with_html);
+	g_test_add_func("/vilistextum/unchanged-inputs/text-only", test_unchanged_inputs_without_html);
+	g_test_add_func("/vilistextum/changed-inputs/text-only", test_changed_inputs_without_html);
+	return g_test_run();
+}
